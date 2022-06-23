b13c4085c99950f12aa23397eec696022f0d899d
NganNguyen <ngan.ntk13@pnj.com.vn>
2022-06-23 10:29:08 +0700

    1st upload

---
+++ SQL/Dax Evaluattion.sql
@@ -0,0 +1,4 @@
+Evaluate 
+(CALCULATETABLE ( SELECTCOLUMNS ('Fact - All Sales Invoice Line',
+	"abc",'Fact - All Sales Invoice Line'[Customer Type (Month)]),
+		'Fact - All Sales Invoice Line'[Invoice Number]= "9002919564"))
