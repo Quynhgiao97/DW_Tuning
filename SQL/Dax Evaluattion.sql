7b6107b2d79fe4f1124b5b147ed5db9af4268ac9
Quynh <97425586+Quynhgiao97@users.noreply.github.com>
2022-06-23 08:51:54 +0700

    Delete SQL directory

--- SQL/Dax Evaluattion.sql
+++
@@ -1,4 +0,0 @@
-Evaluate 
-(CALCULATETABLE ( SELECTCOLUMNS ('Fact - All Sales Invoice Line',
-	"abc",'Fact - All Sales Invoice Line'[Customer Type (Month)]),
-		'Fact - All Sales Invoice Line'[Invoice Number]= "9002919564"))
